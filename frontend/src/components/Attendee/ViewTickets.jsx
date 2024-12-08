a87cebe23b1020494d3b21d1afe33ddd3876db05
Abdullah-57 <i222626@nu.edu.pk>
2024-12-08 14:30:59 +0500

    Viewing Tickets for Registered Events

--- frontend/src/components/Attendee/ViewTickets.jsx
+++ frontend/src/components/Attendee/ViewTickets.jsx
@@ -1,3 +1,5 @@
+// View ing Tickets for all registered events
+
 import React, { useState, useEffect } from "react";
 import { FaTicketAlt } from "react-icons/fa";
 import { MdEvent, MdDateRange } from "react-icons/md";
